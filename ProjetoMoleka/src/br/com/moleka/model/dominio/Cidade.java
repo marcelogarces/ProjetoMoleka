4519e72fd37e8907a5828ac0660ed075632c00ef
Marcelo Garces Guilherme <marcelo.rj.ni@gmail.com>
2013-08-16 00:07:44 -0300

    Sobre escrita do método hasCode e equals

--- ProjetoMoleka/src/br/com/moleka/model/dominio/Cidade.java
+++ ProjetoMoleka/src/br/com/moleka/model/dominio/Cidade.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Entity
@@ -23,7 +24,7 @@ public class Cidade implements Serializable{
 	
 	@Override
 	public String toString() {
-		return "Cidade [nome=" + nome + "]";
+		return  nome;
 	}
 	@ManyToOne
 	private Estado estado;
@@ -47,6 +48,28 @@ public class Cidade implements Serializable{
 	public void setEstado(Estado estado) {
 		this.estado = estado;
 	}
-	
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
 }
--- ProjetoMoleka/src/br/com/moleka/model/dominio/Estado.java
+++ ProjetoMoleka/src/br/com/moleka/model/dominio/Estado.java
@@ -16,18 +16,18 @@ public class Estado implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer id;
+	private Long id;
 	private String nome;
 	private String uf;
 	
 	@ManyToOne
 	private Pais pais;
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
@@ -57,7 +57,33 @@ public class Estado implements Serializable {
 
 	@Override
 	public String toString() {
-		return  uf;
+		return  nome;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Estado other = (Estado) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
 
 }
