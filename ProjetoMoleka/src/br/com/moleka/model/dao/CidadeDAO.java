7af00ee6e5b0cfca7c27de168cd7579c9172ef4a
Marcelo Garces Guilherme <marcelo.rj.ni@gmail.com>
2013-08-16 00:30:59 -0300

    Acerto

--- ProjetoMoleka/src/br/com/moleka/model/dao/CidadeDAO.java
+++ ProjetoMoleka/src/br/com/moleka/model/dao/CidadeDAO.java
@@ -1,5 +1,6 @@
 package br.com.moleka.model.dao;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -7,23 +8,32 @@ import javax.persistence.Query;
 import br.com.moleka.model.dominio.Cidade;
 import br.com.moleka.model.dominio.Estado;
 
-public class CidadeDAO {
-	
+public class CidadeDAO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private EntityManager entityManager;
-	private DAO<Cidade> dao; 
-	
-	public CidadeDAO(EntityManager entityManager){
+	private DAO<Cidade> dao;
+
+	public CidadeDAO(EntityManager entityManager) {
 		this.entityManager = entityManager;
-		dao = new DAOImpl<Cidade>(Cidade.class,entityManager);
+		dao = new DAOImpl<Cidade>(Cidade.class, entityManager);
 	}
-	
-	public List<Cidade>obterCidadePorEstado(Estado estado){
-		Query query = entityManager.createQuery("from Cidade  c where c.estado = :estado ");
-		query.setParameter("estado",estado);
+
+	public Cidade obterCidadePorId(Long id) {
+		return dao.getBean(id);
+	}
+
+	public List<Cidade> obterCidadePorEstado(Estado estado) {
+		Query query = entityManager
+				.createQuery("from Cidade  c where c.estado = :estado ");
+		query.setParameter("estado", estado);
 		return query.getResultList();
 	}
-	
-	public List<Cidade>listarCidades(){
-	   return dao.listarTodos();	
+
+	public List<Cidade> listarCidades() {
+		return dao.listarTodos();
 	}
 }
