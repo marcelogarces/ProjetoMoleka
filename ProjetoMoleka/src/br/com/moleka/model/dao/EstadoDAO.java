179e5485594cea8c51358009811e9c178c855beb
Marcelo Garces Guilherme <marcelo.rj.ni@gmail.com>
2013-08-16 00:32:11 -0300

    Implementação da serialização e método para obter estado por id

--- ProjetoMoleka/src/br/com/moleka/model/dao/EstadoDAO.java
+++ ProjetoMoleka/src/br/com/moleka/model/dao/EstadoDAO.java
@@ -1,11 +1,17 @@
 package br.com.moleka.model.dao;
 
+import java.io.Serializable;
 import java.util.List;
 import javax.persistence.EntityManager;
 import br.com.moleka.model.dominio.Estado;
 
-public class EstadoDAO {
+public class EstadoDAO implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private EntityManager entityManager;
 	
 	private DAO<Estado> dao; 
@@ -18,6 +24,10 @@ public class EstadoDAO {
 
 	public List<Estado>listarEstados(){
 		return dao.listarTodos();
+	}
+	
+	public Estado obterEstadoPorId(Long id){
+		return dao.getBean(id);
 	}
 	
 	
